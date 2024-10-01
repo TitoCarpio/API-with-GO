c3115adb38df29437b5de099297d878ed55b105f
TitoCarpio <00226620@uca.edu.sv>
2024-10-01 15:36:32 -0600

    creates the server for connection

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func main() {
-	mux.NewRouter()
+	// Metodo para crear un router
+	router := mux.NewRouter()
+
+	router.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) { //los parametros de la funcion son: writer que sera la forma en la que se respondera al clente y el read es para pode tener acceso a los parametros que me envien
+		w.Write([]byte("Hello Word"))
+
+	})
+	//inicializando el servidor
+	http.ListenAndServe(":3000", router) //puerto en donde se va a ejecutar y el segundo es el router que creamos
 }
